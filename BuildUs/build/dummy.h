6d154e4c37e4600bd1472630c33b4c7173dce011
Antoine GÉLIN <gelinantoine@gmail.com>
2020-02-18 19:54:58 -0500

    clean²

--- BuildUs/build/dummy.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// AB - pourquoi c'est encore ici?
-
-#include <boost/filesystem.hpp>
-
-int GetAnswerToLifeUniverseAndEverything();
-void TestDeTrucs() throw (boost::filesystem::filesystem_error);
