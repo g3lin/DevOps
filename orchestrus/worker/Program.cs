d8a5505d748b6d14ed2ca79f6bd164a3cc471e7e
Antoine GÉLIN <gelinantoine@gmail.com>
2020-03-31 12:54:51 -0400

    Organisation dépot et début travail worker

---
+++ orchestrus/worker/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Docker.DotNet;
+
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+using System.Threading;
+
+
+/**
+
+On va utiliser le SDK de docker en C# par MS
+https://github.com/microsoft/Docker.DotNet
+
+Le service de sockets est inspiré par 
+https://docs.microsoft.com/fr-fr/dotnet/framework/network-programming/asynchronous-server-socket-example
+
+**/
+
+namespace worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DockerClient client = new DockerClientConfiguration(
+                new Uri("unix:///var/run/docker.sock"))
+            .CreateClient();
+            
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+        static void createContainer(DockerClient client){
+
+
+        }
+    }
+
+
+
+
+   
+
+
+        
+
+}
---
+++ orchestrus/worker/Readme.md
@@ -0,0 +1,1 @@
+# Partie controle des conteneurs
