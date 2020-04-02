1efa62593a64e3785a28319148dc60cfe21bc29c
Antoine GÉLIN <gelinantoine@gmail.com>
2020-04-02 09:50:06 -0400

    Ajouts sockets rudimentaires  et demarrage d'un conteneur

--- orchestrus/worker/Program.cs
+++ orchestrus/worker/Program.cs
@@ -17,9 +17,6 @@ using System.IO;
 On va utiliser le SDK de docker en C# par MS
 https://github.com/microsoft/Docker.DotNet
 
-Le service de sockets est inspiré par 
-https://docs.microsoft.com/fr-fr/dotnet/framework/network-programming/asynchronous-server-socket-example
-
 **/
 
 namespace worker
@@ -28,43 +25,52 @@ namespace worker
     {
         static async Task Main(string[] args)
         {
+            // Création du client Docker
             DockerClient client = new DockerClientConfiguration(
                 new Uri("unix:///var/run/docker.sock"))
             .CreateClient();
 
-            Task.Run(async () =>
+            SocketsManager socket = new SocketsManager();
+            //SocketsManager.StartListening();
+
+            // Pull de l'image
+            ImagesCreateParameters image = new ImagesCreateParameters
             {
-                await client.Images.CreateImageAsync(
-                new ImagesCreateParameters
-                {
-                    FromImage = "alpine",
-                    Tag = "3.11.5",
-                },
-                 new AuthConfig(), new Progress<JSONMessage>(), CancellationToken.None);
-
-
-            }).GetAwaiter().GetResult();
-
-            
-
-            
-            
-            // Console.WriteLine("Hello World!");
+                FromImage = "hello-seattle",
+                Tag = "linux",
+            };
+            await createContainer(client, image);
+            bool status = await startContainer(client, "hello-world", new string[0]);
+            Console.WriteLine(status.ToString());
         }
 
 
 
-        static async void createContainer(DockerClient client){
-                        
+        static async Task createContainer(DockerClient client, ImagesCreateParameters image){
+            await client.Images.CreateImageAsync(image, new AuthConfig(), new Progress<JSONMessage>(), CancellationToken.None);
         }
-    }
 
-
-
-
-   
-
-
-        
+        static async Task<bool> startContainer(DockerClient client, string imageName, string[] dockerParams){
+            // On va d'abbord creer un conteneur avec les bons paramètres
+            var parameters = new Config {
+              Image = imageName,              
+              ArgsEscaped = false,
+              AttachStderr = false,
+              AttachStdin = false,
+              AttachStdout = true,
+              Cmd = dockerParams
+            };
+            CreateContainerResponse response = await client.Containers.CreateContainerAsync(new CreateContainerParameters(parameters));
+            
+            //Et ensuite lancer ce qu'on a créé
+            bool status = await client.Containers.StartContainerAsync(response.ID, new ContainerStartParameters(),CancellationToken.None);
+            return status;
+        }
+    }   
 
 }
+
+
+
+
+
