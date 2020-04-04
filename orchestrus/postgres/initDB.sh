a54b6a980db4f0901e8fc95723aa06d8862666cd
Antoine GÉLIN <gelinantoine@gmail.com>
2020-04-04 09:03:42 -0400

    add custom postgres

---
+++ orchestrus/postgres/initDB.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname 'IGL601' <<-EOSQL
+    CREATE TABLE worker (
+        ip VARCHAR (15) PRIMARY KEY,
+        port VARCHAR (4)
+    );
+
+    CREATE TABLE image (
+        name VARCHAR (50) PRIMARY KEY,
+        docker_id VARCHAR (50),
+        worker_ip VARCHAR (15)
+        CONSTRAINT worker_ip_fkey FOREIGN KEY (worker_ip)
+            REFERENCES worker (ip)
+    );
+
+EOSQL
