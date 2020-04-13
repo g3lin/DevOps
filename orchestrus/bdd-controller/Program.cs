d8550fc97cc17ec0e623c859b5df635b8dba8cd7
Antoine GÉLIN <gelinantoine@gmail.com>
2020-04-13 09:23:40 -0400

    MàJ appels BDD

--- orchestrus/bdd-controller/Program.cs
+++ orchestrus/bdd-controller/Program.cs
@@ -69,7 +69,8 @@ namespace bdd_controller
             //     return reader.GetString(0);
             // }
 
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT some_field FROM data", conn);
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM image WHERE worker_ip = @wip ;", conn);
+            cmd.Parameters.AddWithValue("wip", root.GetProperty("ipWorker").ToString());
             var reader = cmd.ExecuteReader();
             reader.Read();
             return reader.GetString(0);
@@ -79,7 +80,7 @@ namespace bdd_controller
 
         private string DBListWorkers(JsonElement root){
 
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM worker WHERE", conn);
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM worker;", conn);
             var reader = cmd.ExecuteReader();
             reader.Read();
             return reader.GetString(0);
@@ -89,8 +90,16 @@ namespace bdd_controller
 
         private bool DBUpdateImage(JsonElement root){
            
-            var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn);
-            cmd.Parameters.AddWithValue("p", "Hello world");
+            var cmd = new NpgsqlCommand("INSERT INTO image (name, docker_id, worker_ip, worker_port, image_ports)"+
+                                        " VALUES (@name, @dID, @wIP, @wPort, @imPorts);", conn);
+           
+            cmd.Parameters.AddWithValue("name", root.GetProperty("nomImage").ToString());
+            cmd.Parameters.AddWithValue("dID", root.GetProperty("idImage").ToString());
+            cmd.Parameters.AddWithValue("wIP", root.GetProperty("ipWorker").ToString());
+            cmd.Parameters.AddWithValue("wPort", root.GetProperty("workerPort").ToString());
+            cmd.Parameters.AddWithValue("imPorts", root.GetProperty("ImagePorts").ToString());
+
+            
             int ret = cmd.ExecuteNonQuery();
             
             if (ret == -1 || ret == 0) return false;
@@ -100,8 +109,13 @@ namespace bdd_controller
 
         private bool DBUpdateWorker (JsonElement root){
 
-            var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn);
-            cmd.Parameters.AddWithValue("p", "Hello world");
+            var cmd = new NpgsqlCommand("INSERT INTO worker (ip, port, status) VALUES (@ip, @port, @status)"+
+                                        "ON CONFLICT (ip) DO UPDATE  SET status = EXCLUDED.status ;", conn);
+            
+            cmd.Parameters.AddWithValue("ip", root.GetProperty("ip").ToString());
+            cmd.Parameters.AddWithValue("port", root.GetProperty("workerPort").ToString());
+            cmd.Parameters.AddWithValue("status", root.GetProperty("status").ToString());
+
             int ret = cmd.ExecuteNonQuery();
             
             if (ret == -1 || ret == 0) return false;
--- orchestrus/postgres/initDB.sh
+++ orchestrus/postgres/initDB.sh
@@ -12,6 +12,8 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname 'IGL601' <<-EOSQL
         name VARCHAR (50) PRIMARY KEY,
         docker_id VARCHAR (50),
         worker_ip VARCHAR (15),
+        worker_port INTEGER,
+        image_ports VARCHAR (15)
         CONSTRAINT worker_ip_fkey FOREIGN KEY (worker_ip)
             REFERENCES worker (ip)
     );
