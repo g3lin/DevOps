e80d72c4c521b898b7442f55086611edcafd67b8
Antoine GÉLIN <gelinantoine@gmail.com>
2020-04-15 13:55:46 -0400

    Ajout des tests

---
+++ orchestrus/worker/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace worker
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
